b7982f9ec90db4449608727ae94f4e0b65761886
Matías Salinas <salinasmatias1990@gmail.com>
2021-11-22 01:46:55 -0300

    Products dto now include product id

--- TopChoiceHardware.Products.Domain/DTOs/ProductDtoForDisplay.cs
+++ TopChoiceHardware.Products.Domain/DTOs/ProductDtoForDisplay.cs
@@ -8,6 +8,7 @@ namespace TopChoiceHardware.Products.Domain.DTOs
 {
     public class ProductDtoForDisplay
     {
+        public int ProductId { get; set; }
         public string ProductName { get; set; }
         public string CategoryName { get; set; }
         public string SupplierName { get; set; }
